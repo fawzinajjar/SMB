432df712317b20986fa4521ecedf631c14e74e0c
Fawzi Najjar <fawzinajjar@icloud.com>
2021-09-08 19:41:58 +0300

    updates on routes

---
+++ api/signup.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    console.log("signup!!")
+})
+module.exports = router
+
+
--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const config = require("./config.json")
 const experss = require("express")
-
 const app = experss()
-
+// Routes 
+const signin = require("./api/signin")
+const signup = require("./api/signup")
+// Server 
+app.listen(3000,() => console.log(`>> Server started on port : ${config.PORT}`))
+// Tests
 app.get("/", ( req, res ) => {
 res.send("hello world!")
 })
-
-app.listen(3000,() => console.log(`>> Server started on port : ${config.PORT}`))
+// MiddleWare
+app.use("/signin", signin)
+app.use("/signup", signup)
